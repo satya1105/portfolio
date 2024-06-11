3b8672458a4788b501bc7be45a4ad87894ebef34
satya1105 <satya.ranjan.vit@gmail.com>
2024-06-11 23:21:36 +0530

    update in portfolio

--- myPortfolio/script.js
+++ myPortfolio/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "MCA","Tech Enthusiast"],
+        strings: ["Developer", "Designer", "Master in computer application","Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", " MCA","Tech Enthusiast"],
+        strings: ["Developer", "Designer", " Master in computer application","Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
